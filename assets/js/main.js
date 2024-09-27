346fc6384eda832183f3a14870cd58c2206d8a83
Emiliano Rotta <emilianorottaf@gmail.com>
2024-09-27 19:30:53 -0300

    ejercicios para hacer

--- assets/js/main.js
+++ assets/js/main.js
@@ -30,3 +30,22 @@
 
 // frutasSet.clear()
 // console.log(frutasSet.size) //0
+
+//--------------------------------------------------------------------
+//Ejercicios para hacer:
+//--------------------------------------------------------------------
+
+// Consigna 1: Crear un Set de números únicos 
+//Crea un Set que almacene números únicos. Agrega al menos 5 números usando el método add(), intenta agregar un número duplicado y luego imprime la cantidad de elementos usando la propiedad size. Muestra el contenido final del Set.
+
+// Consigna 2: Verificar la existencia de un valor 
+//Crea un Set con los nombres de tus 3 frutas favoritas. Luego, usa el método has() para verificar si una fruta específica está en el Set. Si está, imprime un mensaje que confirme su existencia, y si no está, agrega la fruta al Set.
+
+// Consigna 3: Eliminar elementos de un Set 
+// Crea un Set con los nombres de 5 lenguajes de programación. Luego elimina uno de los lenguajes utilizando el método delete(). Finalmente, imprime el contenido actualizado del Set y la cantidad de elementos restantes.
+
+// Consigna 4: Vaciar un Set 
+//Crea un Set con al menos 4 nombres de ciudades. Luego, utiliza el método clear() para vaciar el Set. Imprime un mensaje confirmando que el Set está vacío.
+
+// Consigna 5: Combinación de operaciones en un Set 
+//Crea un Set vacío. A continuación, usa un ciclo for para agregar los números del 1 al 10 al Set. Luego, elimina los números pares con otro ciclo for y verifica cuántos números impares quedan en el Set. Usa los métodos add(), delete(), size y has() para resolverlo.
